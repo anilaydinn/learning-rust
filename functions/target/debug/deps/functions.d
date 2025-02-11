236e30a4cb4dc8f8799c5b73416ed5a41f9d8109
Anil Aydin <anlaydinn22@gmail.com>
2025-02-11 08:32:21 +0300

    add functions

---
+++ functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\thracian\development\rust-projects\functions\target\debug\deps\functions.exe: src\main.rs
+
+C:\Users\thracian\development\rust-projects\functions\target\debug\deps\functions.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\thracian\development\rust-projects\functions\target\debug\functions.exe: C:\Users\thracian\development\rust-projects\functions\src\main.rs
