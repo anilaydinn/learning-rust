51afe337a7f16ce4195ea61d4d47a69513406f08
Anil Aydin <anlaydinn22@gmail.com>
2025-02-14 21:25:35 +0300

    Add loops

---
+++ loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+C:\Users\thracian\development\rust-projects\loops\target\debug\deps\loops.exe: src\main.rs
+
+C:\Users\thracian\development\rust-projects\loops\target\debug\deps\loops.d: src\main.rs
+
+src\main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+C:\Users\thracian\development\rust-projects\loops\target\debug\loops.exe: C:\Users\thracian\development\rust-projects\loops\src\main.rs
