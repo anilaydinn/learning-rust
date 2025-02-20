2f5d4d9ea9c14d59f37bb18a58ba0289c8c5bcf0
Anil Aydin <anil_aydinn@outlook.com>
2025-02-20 06:38:07 +0300

    add unit like struct

---
+++ unit-like-structs/target/debug/deps/unit_like_structs-35d55446df75fc2e.d
@@ -0,0 +1,5 @@
+C:\Users\thracian\development\rust-projects\unit-like-structs\target\debug\deps\libunit_like_structs-35d55446df75fc2e.rmeta: src\main.rs
+
+C:\Users\thracian\development\rust-projects\unit-like-structs\target\debug\deps\unit_like_structs-35d55446df75fc2e.d: src\main.rs
+
+src\main.rs:
---
+++ unit-like-structs/target/debug/deps/unit_like_structs-9963e02a264a1bcd.d
@@ -0,0 +1,5 @@
+C:\Users\thracian\development\rust-projects\unit-like-structs\target\debug\deps\libunit_like_structs-9963e02a264a1bcd.rmeta: src\main.rs
+
+C:\Users\thracian\development\rust-projects\unit-like-structs\target\debug\deps\unit_like_structs-9963e02a264a1bcd.d: src\main.rs
+
+src\main.rs:
